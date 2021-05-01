1f2a292189feab270faae1b40736791ba4385f39
Jaime Campillay <jcampillayworks@gmail.com>
2021-05-01 11:50:26 -0400

    Avance en ventana views

--- book_authors_proj/apps/book_authors_app/urls.py
+++ book_authors_proj/apps/book_authors_app/urls.py
@@ -4,11 +4,11 @@ from . import views
 urlpatterns = [
 
     path('', views.books),
-    path('add_book/', views.add_book),
-    path('books/', views.viewbook).
-    path('add_auth_to_book/<book_id>/', views.add_auth_to_book),
-    path('authors/', views.authors),
-    path('add_author/', views.add_author),
-    path('authors/<author_id>/', views.view_author),
-    path('add_book_to_auth/<author_id>/', views.add_book_to_auth),
+    path('add_book', views.add_book),
+    path('books/<book_id>', views.view_book),
+    # path('add_auth_to_book/<book_id>/', views.add_auth_to_book),
+    # path('authors/', views.authors),
+    # path('add_author/', views.add_author),
+    # path('authors/<author_id>/', views.view_author),
+    # path('add_book_to_auth/<author_id>/', views.add_book_to_auth),
 ]
--- book_authors_proj/apps/book_authors_app/views.py
+++ book_authors_proj/apps/book_authors_app/views.py
@@ -2,60 +2,23 @@ from django.shortcuts import render, redirect
 from .models import Author, Book
 
 def books(request):
-    data = {
+    data={
         "all_books": Book.objects.all()
     }
-    return render(request, "books_authors_app/books.html", data)
+    return render(request, 'books.html', data)
 
 def add_book(request):
     if request.POST['title'] and request.POST['desc']:
         Book.objects.create(title=request.POST['title'], desc=request.POST['desc'])
-    return redirect("/")
+    return redirect('/')
 
-def view_book(request,book_id):
-    thisBook = Book.objects.get(id=book_id)
-    assoc_authors = thisBook.authors.all()
-    non_assoc_authors = Author.objects.exclude(books=thisBook)
-    data = {
-        "thisBook": thisBook,
-        "assoc_authors": assoc_authors,
-        "non_assoc_authors": non_assoc_authors
+def view_book(request, book_id):
+    this_book = Book.objects.get(id=book_id)
+    associate_author = this_book.authors.all()
+    non_associate_author = Author.objects.exclude(books=this_book)
+    ctx ={
+        'this_book': this_book,
+        'associate_author': associate_author,
+        'non_associate_author': non_associate_author
     }
-    return render(request, "books_authors_app/book.html", data)
-
-def add_auth_to_book(request,book_id):
-    if request.POST['auth_id']:
-        thisBook = Book.objects.get(id=book_id)
-        thisAuthor = Author.objects.get(id=request.POST['auth_id'])
-        thisBook.authors.add(thisAuthor)
-    return redirect("/books/"+book_id)
-
-# Authors Functions
-def authors(request):
-    data = {
-        "all_authors": Author.objects.all()
-    }
-    return render(request, "books_authors_app/authors.html", data)
-
-def add_author(request):
-    if request.POST['fname'] and request.POST['lname'] and request.POST['notes']:
-        Author.objects.create(first_name=request.POST['fname'], last_name=request.POST['lname'], notes=request.POST['notes'])
-    return redirect("/authors")
-
-def view_author(request,author_id):
-    thisAuthor = Author.objects.get(id=author_id)
-    assoc_books = thisAuthor.books.all()
-    non_assoc_books = Book.objects.exclude(authors=thisAuthor)
-    data = {
-        "thisAuthor": thisAuthor,
-        "assoc_books": assoc_books,
-        "non_assoc_books": non_assoc_books
-    }
-    return render(request, "books_authors_app/author.html", data)
-
-def add_book_to_auth(request,author_id):
-    if request.POST['book_id']:
-        thisAuthor = Author.objects.get(id=author_id)
-        thisBook = Book.objects.get(id=request.POST['book_id'])
-        thisAuthor.books.add(thisBook)
-    return redirect("/authors/"+author_id)
+    return render(request, "book.html", ctx)
